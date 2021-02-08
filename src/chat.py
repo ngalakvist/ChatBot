43a2395428a72934aa745812cac8cf3e4144714f
Ngala Talla <ngala.talla@mdh.se>
2021-02-08 16:54:19 +0100

    First_Version

---
+++ src/chat.py
@@ -0,0 +1,71 @@
+import nltk
+from nltk import stem
+from nltk.stem.lancaster import LancasterStemmer
+from tflearn.layers.core import activation
+stemmer = LancasterStemmer()
+ 
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+#Get data
+with open("c:/ML/chat_bot/data/intents-mdh.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+# Preprocessing 
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_x.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+#Bag of words [ 0,2,,0,0,0,0]
+training = []
+output = []
+out_empty = [0 for _ in range(len(labels))]
+for x, doc in enumerate(docs_x):
+    bag =[]
+    wrds = [stemmer.stem(w) for w in doc]
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])]= 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+#AI Model
+net = tflearn.input_data(shape=[None,len(training[0])])
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,8)
+net = tflearn.fully_connected(net,len(output[0]),activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training,output,n_epoch=1000,batch_size=8, show_metric=True)
+
+model.save("model.tflearn")
+         
