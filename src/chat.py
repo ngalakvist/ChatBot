d32e139430536790b1034e575bb5532e90a5b81e
Ngala Talla <ngala.talla@mdh.se>
2021-02-09 14:07:48 +0100

    all in plac

--- src/chat.py
+++ src/chat.py
@@ -15,55 +15,55 @@ import pickle
 with open("c:/ML/chat_bot/data/intents-mdh.json") as file:
     data = json.load(file)
 
-""" try:
+try:
     with open("data.pickle", "rb") as f:
         words,labels,training,output = pickle.load(f)
-except:  """
-words = []
-labels = []
-docs_x = []
-docs_y = []
+except: 
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-# Preprocessing 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
+    # Preprocessing 
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
-labels = sorted(labels)
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
 
-#Bag of words [ 0,2,,0,0,0,0]
-training = []
-output = []
-out_empty = [0 for _ in range(len(labels))]
+    #Bag of words [ 0,2,,0,0,0,0]
+    training = []
+    output = []
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag =[]
-    wrds = [stemmer.stem(w) for w in doc]
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+    for x, doc in enumerate(docs_x):
+        bag =[]
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])]= 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])]= 1
 
-    training.append(bag)
-    output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
-training = numpy.array(training)
-output = numpy.array(output)
+    training = numpy.array(training)
+    output = numpy.array(output)
 
-with open("data.pickle", "wb") as f:
-    pickle.dump((words,labels,training,output),f)
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words,labels,training,output),f)
 
 #AI Model with tensorflow
 net = tflearn.input_data(shape=[None,len(training[0])])
@@ -73,8 +73,12 @@ net = tflearn.fully_connected(net,len(output[0]),activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-model.fit(training,output,n_epoch=30000,batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+   model.load("model.tflearn") 
+except:
+    model.fit(training,output,n_epoch=2000,batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
 
 #Test and Predict
 def bag_of_word(s,words):
