87c2236b9243e66118e2705c4f2f57d8195d101e
Ngala Talla <ngala.talla@mdh.se>
2021-02-11 13:43:57 +0100

    Adde reqment for deployment

--- src/app.py
+++ src/app.py
@@ -4,8 +4,13 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+class ChatBot(Resource):
+    def get(self,question):
+        ans = chat_bot(question)
+        return {"ans":ans}
  
-@app.route("/")
-def chat():
-    return "Hello Chat bot!"
+api.add_resource(ChatBot, "/chatbot/<string:question>")
 
+if __name__ == "__main__":
+
+    app.run(debug=False)
