10b7ea3785ff3f961c6fba0335617c1963ecd278
Ngala Talla <ngala.talla@mdh.se>
2021-02-15 23:02:15 +0100

    Changes File structure To use for docker

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from chat import chat_bot
 from flask import Flask
 from flask import json
 from flask_restful import Api, Resource
+import sys
+import os
 
 app = Flask(__name__)
 api = Api(app)
+port = 5000
+
+if sys.argv.__len__() > 1:
+    port = sys.argv[1]
+print("You said port is : {} ".format(port))
+
 class ChatBot(Resource):
     def get(self,question):
         ans = chat_bot(question)
@@ -17,5 +25,4 @@ class ChatBot(Resource):
 api.add_resource(ChatBot, "/chatbot/<string:question>")
 
 if __name__ == "__main__":
-
-    app.run(debug=False)
+     app.run(host="0.0.0.0", port=port)
